a15c81742ecccb3c5145e00cd924d6c575d378e9
TC <teresa.j.campbell@gmail.com>
2021-12-02 13:53:36 -0500

    achieved random lowercase

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
 var promptLength= window.prompt ('How many characters would you like in your password? Please enter a number between 8 and 128');
 
 var promptLowercase =window.prompt('Would you like Lowercase letters in your password? Enter "YES" or "NO" to choose.');
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-if (promptLowercase === "YES") {
-  for (var i=0; i < Math.floor(promptLength*.25); i++)
-  var pickLowercase = lowercase;
-  console.log(lowercase);
-}
+if (promptLowercase === "YES") 
+
+var getLowercase =lowercase[Math.floor(Math.random()*lowercase.length)];
+
+  for (var i=0; i<Math.ceil(promptLength*.25); i++) {
+
+  console.log(getLowercase[i]*(promptLength*.25));
+  }
+
+
 var promptUppercase =window.prompt('Would you like Uppercase letters in your password? Enter "YES" or "NO" to choose.');
 
 var promptNumbers =window.prompt('Would you like Numbers in your password? Enter "YES" or "NO" to choose.');
@@ -16,7 +22,7 @@ var promptSpecialCharacters=window.prompt('Would you like Special Characters in 
 
 
 
-var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
 
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
@@ -38,3 +44,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+  
